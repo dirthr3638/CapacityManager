1318253cdc7fec935a14c2d9214c6db2057bb9bf
MooJin <dirthr3638@gmail.com>
2015-04-26 22:38:54 +0900

    fircommit

---
+++ CapacityManager/Common/TimeTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapacityManager.Common
+{
+    class TimeTask
+    {
+        private DateTime GetTimeAtNow()
+        {
+            return DateTime.Now;
+        }
+
+        public double CompareDateDay(bool bCompare, DateTime date1)
+        {
+            DateTime date = GetTimeAtNow();
+
+            TimeSpan ts = date1 - date;
+
+            return ts.TotalDays;
+        }
+
+        public string SuitDateChange(double day)
+        {
+            return string.Format("{0}일 전", day);
+        }
+
+    }
+}
